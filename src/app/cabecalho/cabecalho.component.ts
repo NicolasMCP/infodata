357cd985d67265d2ef22be273e0ee39f9e579baa
NicolasMCP <nicolas.mcp@gmail.com>
2021-03-12 21:48:39 -0300

    começando cabeçalho

---
+++ src/app/cabecalho/cabecalho.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'id-cabecalho',
+  templateUrl: './cabecalho.component.html',
+  styleUrls: ['./cabecalho.component.css']
+})
+export class CabecalhoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
